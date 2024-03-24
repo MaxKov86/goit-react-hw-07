bcaa5741e894e66ec07bc7bbcf1e7c73f1a96f67
MaxKov86 <max.kov86@ukr.net>
2024-03-24 18:00:32 +0200

    Delete console.log

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -56,12 +56,9 @@ export const selectContacts = state => state.contacts.items;
 export const selectError = state => state.contacts.error;
 export const selectIsLoading = state => state.contacts.isLoading;
 
-
-
 export const selectFilteredContacts = createSelector(
 	[selectContacts, selectNameFilter],
 	(contacts, nameFilter) => {
-		console.log('selectFilteredContacts');
 		return contacts.filter(contact =>
 			contact.name.toLowerCase().includes(nameFilter.toLowerCase())
 		);
